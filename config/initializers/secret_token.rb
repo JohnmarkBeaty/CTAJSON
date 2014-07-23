d993215e06afd7208b022ad559c8d9c50faf9c62
Johnmark <johnmarkbeaty@gmail.com>
2014-07-23 05:56:26 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CTAJSON::Application.config.secret_key_base = 'f3b0b4fb30be8e99ab58227a2011a38d5f4e6810e74f0c6b5d9e199be07f34767d7fa0a52f8f40ad14de3c1971b480a632827738ac9ccad1fb5b7c054c8833b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CTAJSON::Application.config.session_store :cookie_store, key: '_CTAJSON_session'
